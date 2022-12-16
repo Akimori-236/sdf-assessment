ce6ae26dd73a072cdec89b1c41f88ebef5b229f1
Wee Seng <wee.bscl@gmail.com>
2022-12-16 12:09:09 +0800

    task02: added cmdline arg check

--- task02/src/calculator/Main.java
+++ task02/src/calculator/Main.java
@@ -2,6 +2,10 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 0){
+            System.err.println("ERROR> This program takes in no command line arguments");
+            return;
+        }
         System.out.println("Welcome.");
         Calculator.startShell();
         System.out.println("Bye bye");
