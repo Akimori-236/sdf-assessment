7ee1ac20f167f52c277999bc15bb50ffcfbb5ce4
Wee Seng <wee.bscl@gmail.com>
2022-12-16 13:08:34 +0800

    task02: added checks for incorrect inputs

--- task02/src/calculator/Calculator.java
+++ task02/src/calculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     public static void startShell() {
         Double last = 0.0d;
         Console cons = System.console();
-        
+
         while (true) {
             String userInput = cons.readLine("> ");
             // EXIT
@@ -20,36 +20,40 @@ public class Calculator {
             }
 
             // GETTING PARAMETERS
-            String[] parameters = userInput.split(" ");
             Double num1 = 0.0d;
             Double num2 = 0.0d;
-            try {
-                num1 = Double.parseDouble(parameters[0]);
-                num2 = Double.parseDouble(parameters[2]);
-                switch (parameters[1]) {
-                    case "+":
-                        last = num1 + num2;
-                        break;
-                    case "-":
-                        last = num1 - num2;
-                        break;
-                    case "*":
-                        last = num1 * num2;
-                        break;
-                    case "/":
-                        last = num1 / num2;
-                        break;
-                    default:
-                        System.err.println("ERROR> Unsupported operator");
-                }
-                if (last % 1 == 0) {
-                    System.out.println(Math.round(last));
-                } else {
-                    System.out.println(last);
-                }
-            } catch (NumberFormatException e) {
+            String[] parameters = userInput.split(" ");
+            if (parameters.length != 3) {
                 System.err.println("ERROR> Invalid numbers/usage\nUsage>>> 1 + 1");
-            }
+            } else {
+                try {
+                    num1 = Double.parseDouble(parameters[0]);
+                    num2 = Double.parseDouble(parameters[2]);
+                    switch (parameters[1]) {
+                        case "+":
+                            last = num1 + num2;
+                            break;
+                        case "-":
+                            last = num1 - num2;
+                            break;
+                        case "*":
+                            last = num1 * num2;
+                            break;
+                        case "/":
+                            last = num1 / num2;
+                            break;
+                        default:
+                            System.err.println("ERROR> Unsupported operator");
+                    }
+                    if (last % 1 == 0) {
+                        System.out.println(Math.round(last));
+                    } else {
+                        System.out.println(last);
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("ERROR> Invalid numbers/usage\nUsage>>> 1 + 1");
+                }
+            } // end if
         }
     }
 }
