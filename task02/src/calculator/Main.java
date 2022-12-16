70e0f33a65c04fe206b852dba366c59048034f8f
Wee Seng <wee.bscl@gmail.com>
2022-12-16 12:11:54 +0800

    task02: added clear screen function on successful program start

--- task02/src/calculator/Main.java
+++ task02/src/calculator/Main.java
@@ -2,10 +2,14 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length != 0){
+        if (args.length != 0) {
             System.err.println("ERROR> This program takes in no command line arguments");
             return;
         }
+        // CLEAR SCREEN
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        // START SHELL
         System.out.println("Welcome.");
         Calculator.startShell();
         System.out.println("Bye bye");
