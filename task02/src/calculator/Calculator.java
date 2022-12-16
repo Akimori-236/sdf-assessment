5285cd4d6b59bce27b078241299d91597e564e89
Wee Seng <wee.bscl@gmail.com>
2022-12-16 12:07:23 +0800

    task02: calculator working

--- task02/src/calculator/Calculator.java
+++ task02/src/calculator/Calculator.java
@@ -4,7 +4,7 @@ import java.io.Console;
 
 public class Calculator {
     public static void startShell() {
-        Double $last = 0d;
+        Double last = 0.0d;
         Console cons = System.console();
 
         while (true) {
@@ -13,25 +13,45 @@ public class Calculator {
             if (userInput.equalsIgnoreCase("exit")) {
                 break;
             }
+            // SUB IN LAST NUMBER
             if (userInput.contains("$last")) {
-                userInput.replace("$last", Double.toString($last));
+                userInput = userInput.replace("$last", Double.toString(last));
+                // System.out.println(userInput);
             }
-            String[] parameters = userInput.split(" ");
-            Double num1 = Double.parseDouble(parameters[0]);
-            Double num2 = Double.parseDouble(parameters[2]);
-            String operator = parameters[1];
 
-            switch (operator) {
-                case "+":
-                    break;
-                case "-":
-                    break;
-                case "*":
-                    break;
-                case "/":
-                    break;
-                default:
-                    System.err.println("ERROR> Unsupported operator");
+            // GETTING PARAMETERS
+            String[] parameters = userInput.split(" ");
+            Double num1 = 0.0d;
+            Double num2 = 0.0d;
+            try {
+                num1 = Double.parseDouble(parameters[0]);
+                num2 = Double.parseDouble(parameters[2]);
+                switch (parameters[1]) {
+                    case "+":
+                        last = num1 + num2;
+                        break;
+                    case "-":
+                        last = num1 - num2;
+                        break;
+                    case "*":
+                        last = num1 * num2;
+                        break;
+                    case "/":
+                        last = num1 / num2;
+                        break;
+                    default:
+                        System.err.println("ERROR> Unsupported operator");
+                }
+                if (last % 1 == 0) {
+                    System.out.println(Math.round(last));
+                } else {
+                    System.out.println(last);
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("ERROR> Invalid numbers/usage\nUsage>>> 1 + 1");
+                // CLEAR SCREEN
+                // System.out.print("\033[H\033[2J");
+                // System.out.flush();
             }
         }
     }
--- task02/src/calculator/Main.java
+++ task02/src/calculator/Main.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome!");
+        System.out.println("Welcome.");
         Calculator.startShell();
         System.out.println("Bye bye");
     }
